725775afb0bb9865e40bf97ca05f804d0e951bdc
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-02-24 02:03:58 +0300

    Completed Sign Up Code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth.ktx)
+    implementation(libs.firebase.database.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/guvenlipati/SecondSignUpFragment.kt
+++ app/src/main/java/com/example/guvenlipati/SecondSignUpFragment.kt
@@ -6,9 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
 import android.widget.Spinner
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.lang.ref.Reference
 
 class SecondSignUpFragment : Fragment() {
+
+    private lateinit var firebaseUser: FirebaseUser
+    private lateinit var databaseReference: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +32,7 @@ class SecondSignUpFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
 
         val spinnerProvince: Spinner = view.findViewById(R.id.spinnerProvince)
         val adapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(
@@ -36,6 +49,66 @@ class SecondSignUpFragment : Fragment() {
             android.R.layout.simple_spinner_item
         )
         spinnerTown.adapter = adapter2
-    }
 
+
+        view.findViewById<Button>(R.id.buttonUserInfoSave).setOnClickListener {
+
+            firebaseUser = FirebaseAuth.getInstance().currentUser!!
+            databaseReference = FirebaseDatabase.getInstance().getReference("users").child(firebaseUser.uid)
+
+            val userName=view.findViewById<EditText>(R.id.editTextUserName)
+            val userSurname=view.findViewById<EditText>(R.id.editTextUserSurname)
+            val radioGroup: RadioGroup = view.findViewById(R.id.radioGroup2)
+            val selectedRadioButtonId: Int = radioGroup.checkedRadioButtonId
+            val userGender: Boolean? = when (selectedRadioButtonId) {
+                R.id.radioButtonMale -> true
+                R.id.radioButtonFemale -> false
+                else -> null
+            }
+            val userProvince=view.findViewById<Spinner>(R.id.spinnerProvince).selectedItem.toString()
+            val userTown=view.findViewById<Spinner>(R.id.spinnerTown).selectedItem.toString()
+
+
+            if (userName.text.isEmpty()){
+                showToast("İsminizi giriniz!")
+                return@setOnClickListener
+            }
+
+            if (userSurname.text.isEmpty()){
+                showToast("Soyadınızı giriniz!")
+                return@setOnClickListener
+            }
+
+            if (userGender==null){
+                showToast("Cinsiyetinizi seçiniz!")
+                return@setOnClickListener
+            }
+
+            if (userProvince==null || userTown==null){
+                showToast("Şehir ve İlçe seçiniz!")
+                return@setOnClickListener
+            }
+
+            val hashMap: HashMap<String, Any> = HashMap()
+            hashMap["userId"]=firebaseUser.uid
+            hashMap["userPhoto"]=""
+            hashMap["userName"]=userName.text.toString()
+            hashMap["userSurname"]=userSurname.text.toString()
+            hashMap["userGender"]=userGender.toString()
+            hashMap["userProvince"]=userProvince.toString()
+            hashMap["userTown"]=userTown.toString()
+
+            databaseReference.setValue(hashMap).addOnCompleteListener{task ->
+                if (task.isSuccessful){
+                    showToast("Profil kaydı başarılı :)")
+                }
+                else{
+                    showToast("Hatalı işlem!")
+                }
+            }
+        }
+    }
+    private fun showToast(message:String){
+        Toast.makeText(requireContext(),message, Toast.LENGTH_SHORT).show()
+    }
 }
