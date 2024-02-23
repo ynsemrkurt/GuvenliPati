3e6d5145a9fd951c5621dbc8ae62ffa2276a9b74
Administrator <batinsavran00@gmail.com>
2024-02-24 01:36:39 +0300

    login options page completed

--- app/src/main/java/com/example/guvenlipati/MainActivity.kt
+++ app/src/main/java/com/example/guvenlipati/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.guvenlipati
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         findViewById<Button>(R.id.signUpButton).setOnClickListener {
             goFragment1()
