e526264a9ec9f9099ea83e34788efe8536af65bc
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-02-24 23:43:27 +0300

    Bug Fixed

--- app/src/main/java/com/example/guvenlipati/MainActivity.kt
+++ app/src/main/java/com/example/guvenlipati/MainActivity.kt
@@ -3,34 +3,29 @@ package com.example.guvenlipati
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.loginPageButton).setOnClickListener {
-            goFragment3()
-        }
 
-        findViewById<Button>(R.id.signUpPageButton).setOnClickListener {
-            goFragment1()
-        }
     }
     fun goFragment1(){
         supportFragmentManager.beginTransaction()
-            .replace(R.id.main, FirstSignUpFragment())
-            .addToBackStack(null)
+            .replace(R.id.fragmentContainerView3, FirstSignUpFragment
+                ())
             .commit()
     }
     fun goFragment2(){
         supportFragmentManager.beginTransaction()
-            .replace(R.id.main, SecondSignUpFragment())
+            .replace(R.id.fragmentContainerView3, SecondSignUpFragment())
             .commit()
     }
     fun goFragment3(){
         supportFragmentManager.beginTransaction()
-            .replace(R.id.main, LoginFragment())
+            .replace(R.id.fragmentContainerView3, LoginFragment())
             .commit()
     }
 }
---
+++ app/src/main/java/com/example/guvenlipati/SplashFragment.kt
@@ -0,0 +1,31 @@
+package com.example.guvenlipati
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class SplashFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.loginPageButton).setOnClickListener {
+            (activity as MainActivity).goFragment3()
+        }
+
+        view.findViewById<Button>(R.id.signUpPageButton).setOnClickListener {
+            (activity as MainActivity).goFragment1()
+        }
+    }
+}
