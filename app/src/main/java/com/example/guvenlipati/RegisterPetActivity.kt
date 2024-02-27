adcc3749c52cb2b8dc7eaf5d936bd352ff74b096
Burak Eskisemerci <eskisemercib27@gmail.com>
2024-02-28 00:55:04 +0300

    Back Buttons Activated

--- app/src/main/java/com/example/guvenlipati/RegisterPetActivity.kt
+++ app/src/main/java/com/example/guvenlipati/RegisterPetActivity.kt
@@ -1,8 +1,11 @@
 package com.example.guvenlipati
 
+import android.app.AlertDialog
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class RegisterPetActivity : AppCompatActivity() {
@@ -17,6 +20,8 @@ class RegisterPetActivity : AppCompatActivity() {
         val buttonPetUnVaccine = findViewById<Button>(R.id.buttonPetUnVaccine)
         var petGender: Boolean? = null
         var petVaccine: Boolean? = null
+
+
 
         buttonPetFemale.setOnClickListener {
             petGender = true
@@ -44,13 +49,42 @@ class RegisterPetActivity : AppCompatActivity() {
 
         buttonPetUnVaccine.setOnClickListener {
             petVaccine = false
-            buttonPetUnVaccine.setBackgroundResource(R.drawable.sign2_edittext_bg2)
-            buttonPetUnVaccine.setTextColor(Color.WHITE)
             buttonPetVaccine.setBackgroundResource(R.drawable.sign2_edittext_bg)
             buttonPetVaccine.setTextColor(Color.BLACK)
+            buttonPetUnVaccine.setBackgroundResource(R.drawable.sign2_edittext_bg2)
+            buttonPetUnVaccine.setTextColor(Color.WHITE)
         }
 
+        findViewById<ImageButton>(R.id.backToSplash).setOnClickListener{
+            showAlertDialog()
+        }
+    }
 
+    private fun showAlertDialog() {
+        val alertDialogBuilder = AlertDialog.Builder(this)
+
+        alertDialogBuilder.setTitle("Emin Misiniz?")
+        alertDialogBuilder.setMessage("Eğer geri dönerseniz kaydınız silinecektir.")
+
+        alertDialogBuilder.setPositiveButton("Sil") { _, _ ->
+            showToast("Kaydınız iptal edildi.")
+
+            val containerId = R.id.fragmentContainerView2
+            val fragmentManager = supportFragmentManager
+            val fragment = AddPetFragment()
+            fragmentManager.beginTransaction().replace(containerId, fragment).commit()
+
+        }
+
+        alertDialogBuilder.setNegativeButton("İptal") { _, _ ->
+            showToast("İptal Edildi")
+        }
+
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+    }
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
 }
