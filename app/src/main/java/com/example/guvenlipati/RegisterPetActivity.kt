4b6cb8d68b0ad5d930c156371a304cfa456122b2
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-02-27 23:20:34 +0300

    Update Add Pet Button

--- app/src/main/java/com/example/guvenlipati/RegisterPetActivity.kt
+++ app/src/main/java/com/example/guvenlipati/RegisterPetActivity.kt
@@ -6,15 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class RegisterPet : AppCompatActivity() {
+class RegisterPetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_register_pet)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
