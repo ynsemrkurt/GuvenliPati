da3bbce0e79a973ecd236e37febb575a21e9a1aa
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-02-28 00:53:10 +0300

    Created Button Function and Selected Array

--- app/src/main/java/com/example/guvenlipati/RegisterPetActivity.kt
+++ app/src/main/java/com/example/guvenlipati/RegisterPetActivity.kt
@@ -2,6 +2,8 @@ package com.example.guvenlipati
 
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,47 +12,63 @@ class RegisterPetActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_pet)
 
+        val petType = intent.getStringExtra("petType")
+
+        val petTypeCombo=findViewById<AutoCompleteTextView>(R.id.typeCombo)
+
+        if (petType=="dog"){
+            val adapter = ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, resources.getStringArray(R.array.dog_types_array))
+            petTypeCombo.setAdapter(adapter)
+        }
+        else if (petType=="cat"){
+            val adapter = ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, resources.getStringArray(R.array.cat_types_array))
+            petTypeCombo.setAdapter(adapter)
+        }
+        else if (petType=="bird"){
+            val adapter = ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, resources.getStringArray(R.array.bird_types_array))
+            petTypeCombo.setAdapter(adapter)
+        }
+        else{
+            finish()
+        }
+
 
         val buttonPetFemale = findViewById<Button>(R.id.buttonPetFemale)
         val buttonPetMale = findViewById<Button>(R.id.buttonPetMale)
         val buttonPetVaccine = findViewById<Button>(R.id.buttonPetVaccine)
         val buttonPetUnVaccine = findViewById<Button>(R.id.buttonPetUnVaccine)
+
         var petGender: Boolean? = null
         var petVaccine: Boolean? = null
 
         buttonPetFemale.setOnClickListener {
             petGender = true
-            buttonPetFemale.setBackgroundResource(R.drawable.sign2_edittext_bg2)
-            buttonPetFemale.setTextColor(Color.WHITE)
-            buttonPetMale.setBackgroundResource(R.drawable.sign2_edittext_bg)
-            buttonPetMale.setTextColor(Color.BLACK)
+            selectMethod(buttonPetFemale,buttonPetMale)
         }
 
         buttonPetMale.setOnClickListener {
             petGender = false
-            buttonPetMale.setBackgroundResource(R.drawable.sign2_edittext_bg2)
-            buttonPetMale.setTextColor(Color.WHITE)
-            buttonPetFemale.setBackgroundResource(R.drawable.sign2_edittext_bg)
-            buttonPetFemale.setTextColor(Color.BLACK)
+            selectMethod(buttonPetMale,buttonPetFemale)
         }
 
         buttonPetVaccine.setOnClickListener {
             petVaccine = true
-            buttonPetVaccine.setBackgroundResource(R.drawable.sign2_edittext_bg2)
-            buttonPetVaccine.setTextColor(Color.WHITE)
-            buttonPetUnVaccine.setBackgroundResource(R.drawable.sign2_edittext_bg)
-            buttonPetUnVaccine.setTextColor(Color.BLACK)
+            selectMethod(buttonPetVaccine,buttonPetUnVaccine)
         }
 
         buttonPetUnVaccine.setOnClickListener {
             petVaccine = false
-            buttonPetUnVaccine.setBackgroundResource(R.drawable.sign2_edittext_bg2)
-            buttonPetUnVaccine.setTextColor(Color.WHITE)
-            buttonPetVaccine.setBackgroundResource(R.drawable.sign2_edittext_bg)
-            buttonPetVaccine.setTextColor(Color.BLACK)
+            selectMethod(buttonPetUnVaccine,buttonPetVaccine)
         }
 
 
+    }
+
+    fun selectMethod(selected:Button,unselected:Button) {
+        selected.setBackgroundResource(R.drawable.sign2_edittext_bg2)
+        selected.setTextColor(Color.WHITE)
+        unselected.setBackgroundResource(R.drawable.sign2_edittext_bg)
+        unselected.setTextColor(Color.BLACK)
     }
 
 }
