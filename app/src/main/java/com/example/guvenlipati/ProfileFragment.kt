1699d50bab72140bc1148a7e52f34c70939fcd22
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-03-01 21:39:00 +0300

    BUG FİX

--- app/src/main/java/com/example/guvenlipati/ProfileFragment.kt
+++ app/src/main/java/com/example/guvenlipati/ProfileFragment.kt
@@ -1,33 +1,27 @@
 package com.example.guvenlipati
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AutoCompleteTextView
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import de.hdodenhof.circleimageview.CircleImageView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var auth: FirebaseAuth
+    private lateinit var firebaseUser: FirebaseUser
+    private lateinit var databaseReference: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,23 +31,39 @@ class ProfileFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+        firebaseUser = auth.currentUser!!
+        databaseReference =
+            FirebaseDatabase.getInstance().getReference("users").child(firebaseUser.uid)
+
+        val profilePhoto = view.findViewById<CircleImageView>(R.id.circleImageProfilePhoto)
+        val userNameEdit = view.findViewById<EditText>(R.id.editTextUserName)
+        val userSurname = view.findViewById<EditText>(R.id.editTextUserSurname)
+        val provinceCombo = view.findViewById<AutoCompleteTextView>(R.id.provinceCombo)
+        val townCombo = view.findViewById<AutoCompleteTextView>(R.id.townCombo)
+
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val user = snapshot.getValue(User::class.java)
+                if (user?.userId == firebaseUser.uid) {
+                    if (user.userPhoto.isEmpty()) {
+                        profilePhoto.setImageResource(R.drawable.men_image)
+                    }else{
+                        val imageUri = Uri.parse(user.userPhoto)
+                        Glide.with(this@ProfileFragment).load(imageUri)
+                            .placeholder(R.drawable.men_image)
+                            .into(profilePhoto)
+                    }
                 }
             }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
