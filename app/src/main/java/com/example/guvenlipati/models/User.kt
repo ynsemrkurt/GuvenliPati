05430a87d5b0d078e9c77176ffccdcc62ab3fb4b
Burak Eskisemerci <eskisemercib27@gmail.com>
2024-03-01 21:38:17 +0300

    User Model Added

---
+++ app/src/main/java/com/example/guvenlipati/models/User.kt
@@ -0,0 +1,12 @@
+package com.example.guvenlipati.models
+
+data class User(
+    val userGender: String = "",
+    val userId: String = "",
+    val userName: String = "",
+    val userPhoto: String = "",
+    val userProvince: String = "",
+    val userRegisterDate: String = "",
+    val userSurname: String = "",
+    val userTown: String = ""
+)
