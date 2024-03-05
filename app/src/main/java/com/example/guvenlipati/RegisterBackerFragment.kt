41daf6846da2e8d094962be52c7999ad46fbb70a
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-03-05 19:40:04 +0300

    Small Algorithmic Changes

--- app/src/main/java/com/example/guvenlipati/RegisterBackerFragment.kt
+++ app/src/main/java/com/example/guvenlipati/RegisterBackerFragment.kt
@@ -1,5 +1,6 @@
 package com.example.guvenlipati
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,8 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
@@ -28,38 +31,55 @@ class RegisterBackerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val editTextFullName= view.findViewById<EditText>(R.id.editTextFullName)
-        val editTextID= view.findViewById<EditText>(R.id.editTextID)
-        val editTextAge= view.findViewById<EditText>(R.id.editTextAge)
-        val editTextAdress= view.findViewById<EditText>(R.id.editTextAdress)
-        val editTextExperience= view.findViewById<EditText>(R.id.editTextExperience)
-        val editTextPetNumber=view.findViewById<EditText>(R.id.editTextPetNumber)
-        val editTextBackerAbout=view.findViewById<EditText>(R.id.editTextBackerAbout)
-        val checkBox=view.findViewById<CheckBox>(R.id.checkBox)
-        val checkBox2=view.findViewById<CheckBox>(R.id.checkBox2)
-        val checkBox3=view.findViewById<CheckBox>(R.id.checkBox3)
-        val ConfirmBackerButton=view.findViewById<Button>(R.id.ConfirmBackerButton)
+        val editTextFullName = view.findViewById<EditText>(R.id.editTextFullName)
+        val editTextID = view.findViewById<EditText>(R.id.editTextID)
+        val editTextAge = view.findViewById<EditText>(R.id.editTextAge)
+        val editTextAdress = view.findViewById<EditText>(R.id.editTextAdress)
+        val editTextExperience = view.findViewById<EditText>(R.id.editTextExperience)
+        val editTextPetNumber = view.findViewById<EditText>(R.id.editTextPetNumber)
+        val editTextBackerAbout = view.findViewById<EditText>(R.id.editTextBackerAbout)
+        val checkBox = view.findViewById<CheckBox>(R.id.checkBox)
+        val checkBox2 = view.findViewById<CheckBox>(R.id.checkBox2)
+        val checkBox3 = view.findViewById<CheckBox>(R.id.checkBox3)
+        val ConfirmBackerButton = view.findViewById<Button>(R.id.ConfirmBackerButton)
+        val progressCard = view.findViewById<View>(R.id.progressCard)
+        val buttonPaws = view.findViewById<ImageButton>(R.id.buttonPaw2)
 
-        auth= FirebaseAuth.getInstance()
-        databaseReference= FirebaseDatabase.getInstance().getReference("identifies")
+        auth = FirebaseAuth.getInstance()
+        databaseReference = FirebaseDatabase.getInstance().getReference("identifies")
 
-        ConfirmBackerButton.setOnClickListener{
-            if(auth.currentUser!=null){
+        ConfirmBackerButton.setOnClickListener {
+            if (auth.currentUser != null) {
+                progressCard.visibility = View.VISIBLE
+                buttonPaws.visibility = View.INVISIBLE
+                ConfirmBackerButton.visibility = View.INVISIBLE
 
                 val hashMap: HashMap<String, Any> = HashMap()
-                hashMap["userID"]=auth.currentUser!!.uid
-                hashMap["fullName"]=editTextFullName.text.toString()
-                hashMap["TC"]=editTextID.text.toString()
-                hashMap["age"]=editTextAge.text.toString()
-                hashMap["adress"]=editTextAdress.text.toString()
-                hashMap["experience"]=editTextExperience.text.toString()
-                hashMap["petNumber"]=editTextPetNumber.text.toString()
-                hashMap["about"]=editTextBackerAbout.text.toString()
+                hashMap["userID"] = auth.currentUser!!.uid
+                hashMap["fullName"] = editTextFullName.text.toString()
+                hashMap["TC"] = editTextID.text.toString()
+                hashMap["age"] = editTextAge.text.toString()
+                hashMap["adress"] = editTextAdress.text.toString()
+                hashMap["experience"] = editTextExperience.text.toString()
+                hashMap["petNumber"] = editTextPetNumber.text.toString()
+                hashMap["about"] = editTextBackerAbout.text.toString()
 
-                databaseReference.setValue(hashMap)
+                databaseReference.setValue(hashMap).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val intent = Intent(requireContext(), MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        showToast("Hatalı işlem!")
+                    }
+                    progressCard.visibility = View.INVISIBLE
+                    buttonPaws.visibility = View.VISIBLE
+                    ConfirmBackerButton.visibility = View.VISIBLE
+                }
             }
         }
+    }
 
-
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
 }
