14ddaf97fdafbf0faa4a6f6139bc5b57ae1c5410
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-03-05 18:54:04 +0300

    Update UI and Open Create Pet Backer Page

--- app/src/main/java/com/example/guvenlipati/HomeFragment.kt
+++ app/src/main/java/com/example/guvenlipati/HomeFragment.kt
@@ -1,10 +1,13 @@
 package com.example.guvenlipati
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+
 class HomeFragment : Fragment() {
 
     override fun onCreateView(
@@ -12,5 +15,15 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val goBackerButton = view.findViewById<Button>(R.id.goBackerButton)
+
+        goBackerButton.setOnClickListener {
+            val intent = Intent(requireContext(), PetBackerActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/guvenlipati/PetBackerActivity.kt
+++ app/src/main/java/com/example/guvenlipati/PetBackerActivity.kt
@@ -9,12 +9,6 @@ import androidx.core.view.WindowInsetsCompat
 class PetBackerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_pet_backer)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
