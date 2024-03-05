1ddb77d11a3ee14f71987bf58b5512932396c112
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-03-05 18:05:15 +0300

    Pet Backer Page Added

---
+++ app/src/main/java/com/example/guvenlipati/PetBackerActivity.kt
@@ -0,0 +1,20 @@
+package com.example.guvenlipati
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PetBackerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pet_backer)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
