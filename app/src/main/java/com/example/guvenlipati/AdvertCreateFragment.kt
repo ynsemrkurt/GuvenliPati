f5691afafd66d1cbf472d977a302a2030fdeb4a9
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-03-07 21:32:10 +0300

    Added Advert Create Page Pet List Function

--- app/src/main/java/com/example/guvenlipati/AdvertCreateFragment.kt
+++ app/src/main/java/com/example/guvenlipati/AdvertCreateFragment.kt
@@ -5,55 +5,63 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.guvenlipati.adapter.PetsAdapter
+import com.example.guvenlipati.adapter.SelectPetsAdapter
+import com.example.guvenlipati.models.Pet
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AdvertCreateFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AdvertCreateFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var auth: FirebaseAuth
+    private lateinit var firebaseUser: FirebaseUser
+    private lateinit var databaseReferencePets: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_advert_create, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BackerFilterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AdvertCreateFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+        firebaseUser = auth.currentUser!!
+        databaseReferencePets = FirebaseDatabase.getInstance().getReference("pets")
+
+        val petRecyclerView = view.findViewById<RecyclerView>(R.id.petRecycleView)
+        petRecyclerView.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+        val selectPetList = ArrayList<Pet>()
+
+        databaseReferencePets.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                selectPetList.clear()
+                for (dataSnapShot: DataSnapshot in snapshot.children) {
+                    val pet = dataSnapShot.getValue(Pet::class.java)
+                    if (pet?.userId == firebaseUser.uid) {
+                        pet.let {
+                            selectPetList.add(it)
+                        }
+                    }
                 }
+
+                val petAdapter = SelectPetsAdapter(requireContext(), selectPetList)
+                petRecyclerView.adapter = petAdapter
             }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
+
+
     }
 }
