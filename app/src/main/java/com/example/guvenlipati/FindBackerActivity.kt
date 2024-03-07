4051c7d70d0b732a0f68016711e91f0320a35925
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-03-07 21:14:13 +0300

    Added Go Fragment and Update UI

--- app/src/main/java/com/example/guvenlipati/FindBackerActivity.kt
+++ app/src/main/java/com/example/guvenlipati/FindBackerActivity.kt
@@ -9,12 +9,6 @@ import androidx.core.view.WindowInsetsCompat
 class FindBackerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_find_backer)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
