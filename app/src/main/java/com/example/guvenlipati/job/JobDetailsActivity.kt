423b58505dcf667c99d0591d9158af6a3136d2af
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-03-09 17:46:31 +0300

    Bug Fixed

--- app/src/main/java/com/example/guvenlipati/job/JobDetailsActivity.kt
+++ app/src/main/java/com/example/guvenlipati/job/JobDetailsActivity.kt
@@ -10,12 +10,6 @@ import com.example.guvenlipati.R
 class JobDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_job_details)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
