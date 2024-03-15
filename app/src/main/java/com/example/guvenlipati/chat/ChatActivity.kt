4de547f2bd32cf191d68b05da6a82e2122c47634
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-03-15 20:29:07 +0300

    Update: Transition To Chat Has Been Made In Job Details

--- app/src/main/java/com/example/guvenlipati/chat/ChatActivity.kt
+++ app/src/main/java/com/example/guvenlipati/chat/ChatActivity.kt
@@ -6,16 +6,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.guvenlipati.R
+import com.example.guvenlipati.job.JobDetailsFragment
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_chat)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val userId = intent.getStringExtra("userId")
+        val fragment = ChatingFragment()
+        val args = Bundle()
+        args.putString("userId", userId)
+        fragment.arguments = args
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView, fragment)
+            .commit()
     }
 }
