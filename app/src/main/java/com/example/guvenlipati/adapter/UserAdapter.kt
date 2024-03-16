f13379d703a3eb47572879902d6659ebfaf568c4
Burak Eskisemerci <eskisemercib27@gmail.com>
2024-03-16 23:48:17 +0300

    Chat List Added

---
+++ app/src/main/java/com/example/guvenlipati/adapter/UserAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.guvenlipati.adapter
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.guvenlipati.R
+import com.example.guvenlipati.chat.ChatActivity
+import com.example.guvenlipati.models.User
+import de.hdodenhof.circleimageview.CircleImageView
+
+class UserAdapter(private val context: Context, private val userList: ArrayList<User>) :
+    RecyclerView.Adapter<UserAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_user, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val user = userList[position]
+        holder.bind(user)
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val textUserName: TextView = view.findViewById(R.id.textViewUsername)
+        private val userImage: CircleImageView = view.findViewById(R.id.imageChatUser)
+        private val textMessage: TextView = view.findViewById(R.id.textViewLastMessage)
+        private val userLayout: LinearLayout = view.findViewById(R.id.userCard)
+
+        fun bind(user: User) {
+            textUserName.text = user.userName
+            Glide.with(context)
+                .load(Uri.parse(user.userPhoto))
+                .placeholder(R.drawable.women_ico)
+                .into(userImage)
+
+            userLayout.setOnClickListener {
+                val intent = Intent(context, ChatActivity::class.java)
+                intent.putExtra("userId", user.userId)
+                context.startActivity(intent)
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/guvenlipati/home/ChatListFragment.kt
+++ app/src/main/java/com/example/guvenlipati/home/ChatListFragment.kt
@@ -1,18 +1,67 @@
 package com.example.guvenlipati.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.guvenlipati.R
+import com.example.guvenlipati.adapter.UserAdapter
+import com.example.guvenlipati.models.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
 
 class ChatListFragment : Fragment() {
+    private lateinit var chatRecyclerView: RecyclerView
+    private lateinit var fragmentContext: Context
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        fragmentContext = context
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_chat_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_chat_list, container, false)
+
+        chatRecyclerView = view.findViewById(R.id.chatRecycleView)
+        chatRecyclerView.layoutManager = LinearLayoutManager(fragmentContext, RecyclerView.VERTICAL, false)
+
+        val userList = ArrayList<User>()
+        val firebaseUser: FirebaseUser? = FirebaseAuth.getInstance().currentUser
+        val databaseReference: DatabaseReference = FirebaseDatabase.getInstance().getReference("users")
+
+        firebaseUser?.let { currentUser ->
+            databaseReference.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    userList.clear()
+                    for (dataSnapshot: DataSnapshot in snapshot.children) {
+                        val user: User? = dataSnapshot.getValue(User::class.java)
+                        user?.let {
+                            if (it.userId != currentUser.uid) {
+                                userList.add(it)
+                            }
+                        }
+                    }
+                    val userAdapter = UserAdapter(fragmentContext, userList)
+                    chatRecyclerView.adapter = userAdapter
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(fragmentContext, "ERROR", Toast.LENGTH_SHORT).show()
+                }
+            })
+        } ?: run {
+            Toast.makeText(fragmentContext, "User is not authenticated", Toast.LENGTH_SHORT).show()
+        }
+
+        return view
     }
 }
