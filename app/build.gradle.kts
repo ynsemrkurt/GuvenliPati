ff25daccb645bc0b1610f93216149873b31f92ca
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-03-17 10:27:46 +0300

    Added : Added Retrofit Libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     implementation(libs.lottie)
     //OkHttpClient
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.12")
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    //Gson
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
