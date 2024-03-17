a705f82bbfdd183d2047a75c499d07200a0b81ba
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-03-17 11:00:43 +0300

    Create : Created Constants

---
+++ app/src/main/java/com/example/guvenlipati/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.guvenlipati
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://fcm.googleapis.com/"
+        const val SERVER_KEY = "AAAAMmtkvqE:APA91bF8esAhEeX-PVcJcsw9xEwxCIcHxEvFyRShwQCl_xBZE07JG5G1wczLEWs1bM8DYBgwzUHw3-Sb4nV0CfyNU_ljKU0U_9oOw6ra9eGsb-5GDFhvbXcCHUpqONw60XO1mHsELyXJ"
+        const val CONTENT_TYPE = "application/json"
+    }
+}
