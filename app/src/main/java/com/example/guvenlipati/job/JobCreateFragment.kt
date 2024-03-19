1a36793b336f1f86af1fb9b67c85526f1fd32b47
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-03-19 14:47:12 +0300

    Update : Update Libs Version

--- app/src/main/java/com/example/guvenlipati/job/JobCreateFragment.kt
+++ app/src/main/java/com/example/guvenlipati/job/JobCreateFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
 import androidx.activity.addCallback
 import androidx.core.content.ContextCompat
