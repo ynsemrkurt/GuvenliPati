35c403749cb3ee43049d595e180711314dd39939
Burak Eskisemerci <eskisemercib27@gmail.com>
2024-03-24 12:49:39 +0300

    Syntax Changed

--- app/src/main/java/com/example/guvenlipati/chat/ProfilePreviewFragment.kt
+++ app/src/main/java/com/example/guvenlipati/chat/ProfilePreviewFragment.kt
@@ -1,22 +1,13 @@
 package com.example.guvenlipati.chat
 
 import android.content.Context
-import android.content.Intent
-import android.graphics.Bitmap
 import android.graphics.Color
-import android.graphics.Matrix
-import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
 import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
-import androidx.appcompat.app.AppCompatActivity
-import androidx.exifinterface.media.ExifInterface
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -25,7 +16,6 @@ import com.example.guvenlipati.adapter.PetsAdapter
 import com.example.guvenlipati.databinding.FragmentProfilePreviewBinding
 import com.example.guvenlipati.models.Pet
 import com.example.guvenlipati.models.User
-import com.example.guvenlipati.splash.LoginFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
@@ -33,10 +23,6 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
-import java.io.ByteArrayOutputStream
-import java.io.IOException
 
 class ProfilePreviewFragment : Fragment() {
     lateinit var binding: FragmentProfilePreviewBinding
@@ -46,8 +32,6 @@ class ProfilePreviewFragment : Fragment() {
     private lateinit var databaseReferencePets: DatabaseReference
     private var fragmentContext: Context? = null
     private var user: User? = null
-    private var request: Int = 2020
-    private var filePath: Uri? = null
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -89,7 +73,7 @@ class ProfilePreviewFragment : Fragment() {
 
         val petList = ArrayList<Pet>()
 
-        reference!!.addListenerForSingleValueEvent(object : ValueEventListener {
+        reference.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 user = snapshot.getValue(User::class.java)
                 if (user!!.userPhoto.isEmpty()) {
@@ -134,7 +118,7 @@ class ProfilePreviewFragment : Fragment() {
                     }
                     val petAdapter = PetsAdapter(requireContext(), petList)
                     binding.petRecycleView.adapter = petAdapter
-
+                    
                 }
             }
 
