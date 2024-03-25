fd93cd720c495b294d81886543001e51d4014892
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-03-25 19:54:20 +0300

    Pending Adverts Upload Function Added

--- app/src/main/java/com/example/guvenlipati/advert/PendingAdvertFragment.kt
+++ app/src/main/java/com/example/guvenlipati/advert/PendingAdvertFragment.kt
@@ -5,56 +5,90 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.guvenlipati.JobsAdapter
 import com.example.guvenlipati.R
+import com.example.guvenlipati.models.Job
+import com.example.guvenlipati.models.Pet
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import java.text.SimpleDateFormat
+import java.util.Date
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PendingAdvertFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PendingAdvertFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pending_advert, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PendingAdvertFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PendingAdvertFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val currentDate = Date()
+
+        val pastAdvertRecycleView=view.findViewById<RecyclerView>(R.id.pendingAdvertRecycleView)
+        pastAdvertRecycleView.layoutManager =
+            LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+
+        val databaseReferenceJobs = FirebaseDatabase.getInstance().getReference("jobs")
+        val databaseReferencePets = FirebaseDatabase.getInstance().getReference("pets")
+
+        val jobList = ArrayList<Job>()
+        val petList = ArrayList<Pet>()
+
+        databaseReferencePets.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(petsSnapshot: DataSnapshot) {
+                petList.clear()
+                for (dataSnapshot: DataSnapshot in petsSnapshot.children) {
+                    val pet = dataSnapshot.getValue(Pet::class.java)
+                    if (pet?.userId == FirebaseAuth.getInstance().currentUser?.uid) {
+                        pet?.let {
+                            petList.add(it)
+                        }
+                    }
                 }
+
+                databaseReferenceJobs.addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(jobsSnapshot: DataSnapshot) {
+                        jobList.clear()
+                        for (dataSnapshot: DataSnapshot in jobsSnapshot.children) {
+                            val job = dataSnapshot.getValue(Job::class.java)
+                            val startDate = SimpleDateFormat("dd/MM/yyyy").parse(job?.jobStartDate)
+                            if (startDate != null) {
+                                if (job?.userID == FirebaseAuth.getInstance().currentUser?.uid && !startDate.before(currentDate)) {
+                                    job?.let {
+                                        jobList.add(it)
+                                    }
+                                }
+                            }
+                        }
+                        val adapter = JobsAdapter(requireContext(),jobList, petList)
+                        pastAdvertRecycleView.adapter = adapter
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        showToast("Hata!")
+                    }
+
+                })
+
             }
+
+            override fun onCancelled(error: DatabaseError) {
+                showToast("Hata!")
+            }
+        })
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
 }
