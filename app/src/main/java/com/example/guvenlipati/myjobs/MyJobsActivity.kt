c27199019951da854bfcba72c5e5806249e13768
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-03-25 16:15:15 +0300

    Added MyJobs Page

---
+++ app/src/main/java/com/example/guvenlipati/myjobs/MyJobsActivity.kt
@@ -0,0 +1,21 @@
+package com.example.guvenlipati.myjobs
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.guvenlipati.R
+
+class MyJobsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_my_jobs)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
