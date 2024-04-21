a5a9f69eaab267b209ea3a44a135a135b1ea32ce
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-04-21 17:45:08 +0300

    Added Past Advert Adapter

---
+++ app/src/main/java/com/example/guvenlipati/adapter/PastAdvertsAdapter.kt
@@ -0,0 +1,78 @@
+package com.example.guvenlipati
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.PopupMenu
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.guvenlipati.job.JobDetailsActivity
+import com.example.guvenlipati.job.JobDetailsFragment
+import com.example.guvenlipati.models.Job
+import com.example.guvenlipati.models.Pet
+
+class PastAdvertsAdapter(
+    private val context: Context,
+    private val jobList: ArrayList<Job>,
+    private val petList: ArrayList<Pet>
+) : RecyclerView.Adapter<PastAdvertsAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_past_advert, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PastAdvertsAdapter.ViewHolder, position: Int) {
+        holder.advertCard.animation = AnimationUtils.loadAnimation(context, R.anim.recyclerview_anim)
+
+        val job = jobList[position]
+
+        val petId = job.petID
+
+        val matchingPet = petList.find { it.petId == petId }
+
+        matchingPet?.let {
+            holder.bind(job, it)
+        }
+    }
+
+    override fun getItemCount(): Int = jobList.size
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val advertCard: LinearLayout = view.findViewById(R.id.advertCard)
+        private val petPhotoImageView = view.findViewById<ImageView>(R.id.petPhotoImageView)
+        private val petNameTextView = view.findViewById<TextView>(R.id.petNameTextView)
+        private val jobTypeTextView= view.findViewById<TextView>(R.id.jobTypeTextView)
+        private val petTypeTextView = view.findViewById<TextView>(R.id.petTypeTextView)
+        private val startDateTextView = view.findViewById<TextView>(R.id.startDateTextView)
+        private val endDateTextView = view.findViewById<TextView>(R.id.endDateTextView)
+        private val locationTextView = view.findViewById<TextView>(R.id.locationTextView)
+
+        fun bind(job: Job, pet: Pet) {
+            when(job.jobType){
+                "feedingJob" -> jobTypeTextView.text = "Besleme"
+                "walkingJob" -> jobTypeTextView.text = "Gezdirme"
+                "homeJob" -> jobTypeTextView.text = "Evde Bakım"
+            }
+            petNameTextView.text = pet.petName
+            petTypeTextView.text = pet.petBreed
+            startDateTextView.text = job.jobStartDate
+            endDateTextView.text = job.jobEndDate
+            locationTextView.text = job.jobProvince + ", " + job.jobTown
+
+            Glide.with(context)
+                .load(pet.petPhoto)
+                .placeholder(R.drawable.default_pet_image_2)
+                .into(petPhotoImageView)
+
+        }
+    }
+}
