7164d15c9a1f1218ba0a95ff73b8390b9a56b800
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-04-22 22:23:19 +0300

    Added binding view in the Rating Activity

--- app/src/main/java/com/example/guvenlipati/myjobs/RatingActivity.kt
+++ app/src/main/java/com/example/guvenlipati/myjobs/RatingActivity.kt
@@ -1,4 +1,4 @@
-package com.example.guvenlipati.job
+package com.example.guvenlipati.myjobs
 
 import android.os.Bundle
 import android.widget.ImageButton
@@ -7,13 +7,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.guvenlipati.R
+import com.example.guvenlipati.databinding.ActivityRatingBinding
 
 class RatingActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityRatingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rating)
+        binding = ActivityRatingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        findViewById<ImageButton>(R.id.backToSplash).setOnClickListener {
+        binding.backToSplash.setOnClickListener {
             onBackPressed()
             finish()
         }
