bbd9c1ca4a2ed563fd83daefb14499c8e9694bab
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-04-23 16:35:41 +0300

    Added List Rating Page and Completed ALL

---
+++ app/src/main/java/com/example/guvenlipati/home/ListRatingActivity.kt
@@ -0,0 +1,21 @@
+package com.example.guvenlipati.home
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.guvenlipati.R
+
+class ListRatingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_list_rating)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
