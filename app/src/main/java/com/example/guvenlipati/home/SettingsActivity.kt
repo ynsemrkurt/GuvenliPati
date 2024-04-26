59b265066d9ae9cc88024229a228ac6d0ba0e5c6
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-04-26 20:42:51 +0300

    Added Settings Page

---
+++ app/src/main/java/com/example/guvenlipati/home/SettingsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.guvenlipati.home
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.guvenlipati.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
