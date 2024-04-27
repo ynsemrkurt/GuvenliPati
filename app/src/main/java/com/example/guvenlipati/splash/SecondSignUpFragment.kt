3eb50d5a8115cce57941ea1e650404225c67d1e1
Burak Eskisemerci <eskisemercib27@gmail.com>
2024-04-27 18:50:28 +0300

    SecondSignUpFragment Code Complexity Improved Again

--- app/src/main/java/com/example/guvenlipati/splash/SecondSignUpFragment.kt
+++ app/src/main/java/com/example/guvenlipati/splash/SecondSignUpFragment.kt
@@ -9,17 +9,11 @@ import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AutoCompleteTextView
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.addCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.exifinterface.media.ExifInterface
 import androidx.fragment.app.Fragment
@@ -35,7 +29,6 @@ import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.storage
-import de.hdodenhof.circleimageview.CircleImageView
 import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.time.LocalDate
