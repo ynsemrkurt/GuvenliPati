83e8db5934f8838ee0e02b3f4a6a33cfb2470e9f
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-05-05 23:35:54 +0300

    Clean Code AdvertActivity

--- app/src/main/java/com/example/guvenlipati/advert/AdvertActivity.kt
+++ app/src/main/java/com/example/guvenlipati/advert/AdvertActivity.kt
@@ -2,54 +2,58 @@ package com.example.guvenlipati.advert
 
 import android.os.Bundle
 import android.widget.ImageButton
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.guvenlipati.R
-import com.example.guvenlipati.databinding.ActivityAdvertBinding
-import com.example.guvenlipati.databinding.ActivityHomeBinding
-import com.example.guvenlipati.home.JobsSplashFragment
-import com.example.guvenlipati.payment.PaymentFragment
 import com.google.android.material.tabs.TabLayout
 
 class AdvertActivity : AppCompatActivity() {
+
+    private val tabLayout: TabLayout by lazy { findViewById(R.id.tabLayout) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_advert)
 
-        goPendingAdvertFragment()
+        goFragment(PendingAdvertFragment())
 
-        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
         val statusType = intent.getStringExtra("statusType")
 
         when (statusType) {
             "pending" -> {
-                goPaymentAdvertFragment()
-                tabLayout.selectTab(tabLayout.getTabAt(1))
+                goFragment(PaymentAdvertFragment())
+                selectTab(1)
             }
+
             "active" -> {
-                goActiveAdvertFragment()
-                tabLayout.selectTab(tabLayout.getTabAt(2))
+                goFragment(ActiveAdvertFragment())
+                selectTab(2)
             }
+
             else -> {
-                goPendingAdvertFragment()
-                tabLayout.selectTab(tabLayout.getTabAt(0))
+                goFragment(PendingAdvertFragment())
+                selectTab(0)
             }
         }
 
 
-        findViewById<TabLayout>(R.id.tabLayout).addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+        findViewById<TabLayout>(R.id.tabLayout).addOnTabSelectedListener(object :
+            TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
                 val position = tab.position
-                when (position) {
-                    0 -> goPendingAdvertFragment()
+                goFragment(
+                    when (position) {
+                        0 -> PendingAdvertFragment()
 
-                    1 -> goPaymentAdvertFragment()
+                        1 -> PaymentAdvertFragment()
 
-                    2 -> goActiveAdvertFragment()
+                        2 -> ActiveAdvertFragment()
 
-                    3 -> goPastAdvertFragment()
-                }
+                        3 -> PastAdvertFragment()
+
+                        else -> PendingAdvertFragment()
+                    }
+                )
             }
 
             override fun onTabUnselected(p0: TabLayout.Tab?) {
@@ -60,40 +64,17 @@ class AdvertActivity : AppCompatActivity() {
         })
 
         findViewById<ImageButton>(R.id.backToSplash).setOnClickListener {
-            onBackPressed()
             finish()
         }
     }
 
-    private fun goPastAdvertFragment() {
+    private fun goFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
-            .replace(
-                R.id.fragmentContainerView, PastAdvertFragment()
-            )
+            .replace(R.id.fragmentContainerView, fragment)
             .commit()
     }
 
-    private fun goActiveAdvertFragment() {
-        supportFragmentManager.beginTransaction()
-            .replace(
-                R.id.fragmentContainerView, ActiveAdvertFragment()
-            )
-            .commit()
-    }
-
-    private fun goPendingAdvertFragment() {
-        supportFragmentManager.beginTransaction()
-            .replace(
-                R.id.fragmentContainerView, PendingAdvertFragment()
-            )
-            .commit()
-    }
-
-    private fun goPaymentAdvertFragment(){
-        supportFragmentManager.beginTransaction()
-            .replace(
-                R.id.fragmentContainerView, PaymentAdvertFragment()
-            )
-            .commit()
+    private fun selectTab(position: Int) {
+        tabLayout.selectTab(tabLayout.getTabAt(position))
     }
 }
