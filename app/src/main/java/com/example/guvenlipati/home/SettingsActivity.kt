6ad9fc42f03ddd5e1f225f944669fa8da9f7f9c2
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-09-19 14:29:16 +0300

    Refactor: Improve Settings Activity Code
    - Improved password change logic with helper functions for validation and readability.
    - Simplified notification permission request with version check.
    
    - Used extension functions for email intent creation and toast messages.
    - Removed unnecessary code and optimized imports.
    - Enhanced code structure and formatting for better maintainability.

--- app/src/main/java/com/example/guvenlipati/home/SettingsActivity.kt
+++ app/src/main/java/com/example/guvenlipati/home/SettingsActivity.kt
@@ -3,143 +3,116 @@ package com.example.guvenlipati.home
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.guvenlipati.databinding.ActivitySettingsBinding
+import com.example.guvenlipati.extensions.createEmailIntent
+import com.example.guvenlipati.extensions.isMatching
+import com.example.guvenlipati.extensions.isValidPassword
+import com.example.guvenlipati.extensions.showToast
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class SettingsActivity : AppCompatActivity() {
 
-    private lateinit var mAuth: FirebaseAuth
+    private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivitySettingsBinding
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        mAuth = FirebaseAuth.getInstance()
+        auth = FirebaseAuth.getInstance()
 
-        binding.confirmPasswordButton.setOnClickListener {
-            handleChangePasswordClick()
-        }
+        setupListeners()
+    }
 
-        binding.backToSplash.setOnClickListener {
-            onBackPressed()
-            finish()
-        }
-
-        binding.notificationChange.setOnClickListener {
-            requestNotificationPermission()
-        }
-
-        binding.contactUsButton.setOnClickListener {
-            sendEmail()
-        }
-
-        binding.whoIsBEGTECH.setOnClickListener {
-            openAboutUsActivity()
+    private fun setupListeners() {
+        with(binding) {
+            confirmPasswordButton.setOnClickListener { handleChangePasswordClick() }
+            backToSplash.setOnClickListener { finish() }
+            notificationChange.setOnClickListener { requestNotificationPermission() }
+            contactUsButton.setOnClickListener { sendEmail() }
+            whoIsBEGTECH.setOnClickListener { openAboutUsActivity() }
         }
     }
 
     private fun handleChangePasswordClick() {
-        val currentPassword = binding.editTextCurrentPassword.text.toString()
-        val newPassword = binding.editTextNewPassword.text.toString()
-        val confirmPassword = binding.editTextConfirmPassword.text.toString()
+        with(binding) {
+            val currentPassword = editTextCurrentPassword.text.toString()
+            val newPassword = editTextNewPassword.text.toString()
+            val confirmPassword = editTextConfirmPassword.text.toString()
 
-        if (areFieldsEmpty(currentPassword, newPassword, confirmPassword)) {
-            showToast("Lütfen tüm alanları doldurunuz!")
-            return
-        }
-
-        if (!isPasswordValid(newPassword, confirmPassword)) {
-            showToast("Şifre en az 8 karakter olmalıdır ve boşluk içermemelidir!")
-            return
-        }
-
-        if (!arePasswordsMatching(newPassword, confirmPassword)) {
-            showToast("Şifreler uyuşmuyor!")
-            return
-        }
-
-        if (currentPassword == newPassword) {
-            showToast("Mevcut şifre ile yeni şifre aynı olamaz!")
-            return
-        }
-
-        mAuth.currentUser?.let { currentUser ->
-            currentUser.email?.let { email ->
-                val credential = EmailAuthProvider.getCredential(email, currentPassword)
-                currentUser.reauthenticate(credential)
-                    .addOnCompleteListener { reauthTask ->
-                        if (reauthTask.isSuccessful) {
-                            currentUser.updatePassword(newPassword)
-                                .addOnCompleteListener { updatePasswordTask ->
-                                    if (updatePasswordTask.isSuccessful) {
-                                        showToast("Şifre başarıyla değiştirildi")
-                                        finish()
-                                    } else {
-                                        showToast("Şifre değiştirilirken bir hata oluştu")
-                                    }
-                                }
-                        } else {
-                            showToast("Mevcut şifre yanlış")
-                        }
-                    }
+            when {
+                areFieldsEmpty(currentPassword, newPassword, confirmPassword) -> showToast("Lütfen tüm alanları doldurunuz!")
+                !newPassword.isValidPassword() -> showToast("Şifre en az 8 karakter olmalıdır ve boşluk içermemelidir!")
+                !newPassword.isMatching(confirmPassword) -> showToast("Şifreler uyuşmuyor!")
+                currentPassword == newPassword -> showToast("Mevcut şifre ile yeni şifre aynı olamaz!")
+                else -> changePassword(currentPassword, newPassword)
             }
         }
     }
 
-    private fun arePasswordsMatching(password1: String, password2: String): Boolean {
-        return password1 == password2
+    private fun changePassword(currentPassword: String, newPassword: String) {
+        val currentUser = auth.currentUser ?: return
+        val email = currentUser.email ?: return
+        val credential = EmailAuthProvider.getCredential(email, currentPassword)
+
+        currentUser.reauthenticate(credential)
+            .addOnCompleteListener { reauthTask ->
+                if (reauthTask.isSuccessful) {
+                    updatePassword(currentUser, newPassword)
+                } else {
+                    showToast("Mevcut şifre yanlış")
+                }
+            }
     }
 
-    private fun areFieldsEmpty(vararg fields: String): Boolean {
-        return fields.any { it.isEmpty() }
+    private fun updatePassword(user: FirebaseUser, newPassword: String) {
+        user.updatePassword(newPassword)
+            .addOnCompleteListener { updatePasswordTask ->
+                if (updatePasswordTask.isSuccessful) {
+                    showToast("Şifre başarıyla değiştirildi")
+                    finish()
+                } else {
+                    showToast("Şifre değiştirilirken bir hata oluştu")
+                }
+            }
     }
 
-    private fun isPasswordValid(vararg passwords: String): Boolean {
-        return passwords.all { it.length >= 8 && !it.contains(" ") }
-    }
+    private fun areFieldsEmpty(vararg fields: String) = fields.any { it.isEmpty() }
 
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
-
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     private fun requestNotificationPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
-            != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
-                REQUEST_NOTIFICATION_PERMISSION_CODE
-            )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    REQUEST_NOTIFICATION_PERMISSION_CODE
+                )
+            } else {
+                showToast("Bildirim izni zaten verilmiş!")
+            }
         } else {
-            showToast("Bildirim izni zaten verilmiş!")
+            showToast("Bildirim izni gerekli değil!")
         }
     }
 
+
     private fun sendEmail() {
-        val intent = Intent(
-            Intent.ACTION_SENDTO,
-            Uri.fromParts("mailto", "yunusemre-kurt@outlook.com", null)
-        )
-        startActivity(Intent.createChooser(intent, "Send email..."))
+        val intent = createEmailIntent("yunusemre-kurt@outlook.com")
+        startActivity(Intent.createChooser(intent, "E-posta gönder..."))
     }
 
     private fun openAboutUsActivity() {
-        val intent = Intent(this, AboutUsActivity::class.java)
-        startActivity(intent)
+        startActivity(Intent(this, AboutUsActivity::class.java))
     }
 
     companion object {
